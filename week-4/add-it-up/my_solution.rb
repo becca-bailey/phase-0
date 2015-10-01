f4e0dcbc3c71c417bd0db5c026631291de3bbbda
Becca Nelson <beccanelson88@gmail.com>
2015-09-30 21:55:51 -0500

    Adds 4.7.1 solution

--- week-4/add-it-up/my_solution.rb
+++ week-4/add-it-up/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with Lars Johnson.
 
 # 0. total Pseudocode
 # make sure all pseudocode is commented out!
@@ -27,18 +27,18 @@
 
 # 1. total initial solution
 
-def total(numbers)
-	sum = 0
-	i = 0
-	while i < numbers.length do 
-		sum = sum + numbers[i]
-		i += 1
-	end
-	puts sum
-	return sum
-end
+# def total(numbers)
+# 	sum = 0
+# 	i = 0
+# 	while i < numbers.length do 
+# 		sum = sum + numbers[i]
+# 		i += 1
+# 	end
+# 	puts sum
+# 	return sum
+# end
 
-total([1,2,3])
+# total([1,2,3])
 
 
 
@@ -46,16 +46,44 @@ total([1,2,3])
 # 3. total refactored solution
 
 
+def total(numbers)
+	numbers.reduce(:+)
+end
+
+
 
 # 4. sentence_maker pseudocode
 # make sure all pseudocode is commented out!
-# Input:
-# Output:
+# Input: array of words
+# Output: a sentence beginning with a capital letter and ending with a period.
 # Steps to solve the problem.
+
+# Find the number of items in the array
+# If it is the first item, capitalize the first letter.
+# If it is the last item, add a period.
+# Print all items.
 
 
 # 5. sentence_maker initial solution
 
+# sentence_maker=[]
+# def sentence_maker(words)
+# text = ""
+# words[0].capitalize!
+# words[words.length-1] = words[words.length-1]+"."
+# text = text + words[0]
+# for i in 1..words.length-1
+#   text = text + " " + words[i].to_s
+#   i += 1
+# end
+# puts text
+# return text
+# end
 
-
-# 6. sentence_maker refactored solution
+# 6. sentence_rspemaker refactored solution
+ 
+def sentence_maker(words)
+	words[0].capitalize!
+	words[words.length-1] = words[words.length-1]+"."
+	return words.join(" ")
+end
