b46041e863d7ce93628d55a40cc1da344c9e56df
Becca Nelson <beccanelson88@gmail.com>
2015-09-30 22:06:00 -0500

    Adds 4.7.1 solution

--- week-4/factorial/my_solution.rb
+++ week-4/factorial/my_solution.rb
@@ -1,9 +1,28 @@
 # Factorial
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with Lars Johnson
 
 
 # Your Solution Below
 def factorial(number)
   # Your code goes here
+
+result = 1
+for i in 1..number
+  result = result * i
+  i =+1
 end
+return result
+end
+
+# factorial 5
+
+# Pseudocode
+
+# Receive provided number
+# Set result equal to one
+# FOR provided number of times
+#   Multiply counter times result and store in result
+#   add 1 to counter
+# REPEAT
+# Return result
