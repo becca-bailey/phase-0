2bfad23a32decc930b3b4b6a2b680ec4617cac33
Becca Nelson <beccanelson88@gmail.com>
2015-10-01 14:22:33 -0500

    Adds 4.6.2

---
+++ week-4/largest-integer/my_solution.rb
@@ -0,0 +1,30 @@
+# Largest Integer
+
+# I worked on this challenge [by myself, with: ].
+
+# largest_integer is a method that takes an array of integers as its input
+# and returns the largest integer in the array
+#
+# +list_of_nums+ is an array of integers
+# largest_integer(list_of_nums) should return the largest integer in the +list_of_nums+
+#
+# If +list_of_nums+ is empty the method should return nil
+
+# Your Solution Below
+def largest_integer(list_of_nums)
+
+
+if list_of_nums == []
+	nil
+else
+	i = 0
+	largest = list_of_nums[0]
+	while i < list_of_nums.length
+		if largest < list_of_nums[i]
+			largest = list_of_nums[i]
+		end
+		i += 1
+	end
+	return largest
+end
+end
