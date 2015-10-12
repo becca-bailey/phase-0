f6403737472dfcbcd1c78807c2eaa766b8180ff2
Becca Nelson <beccanelson88@gmail.com>
2015-10-11 20:11:35 -0500

    Adds group research methods

--- week-5/group-research-methods/methods_spec.rb
+++ week-5/group-research-methods/methods_spec.rb
@@ -1,31 +1,31 @@
-# require_relative "my_solution"
+require_relative "my_solution"
 
-# # PERSON 1: FIND MATCHING DATA
-# describe 'PERSON 1: my_array_finding_method' do
-#   let(:i_want_pets) {["I", "want", 3, "pets", "but", "only", "have", 2 ]}
+# PERSON 1: FIND MATCHING DATA
+describe 'PERSON 1: my_array_finding_method' do
+  let(:i_want_pets) {["I", "want", 3, "pets", "but", "only", "have", 2 ]}
 
-#   it "selects all words with a 't' in the original order" do
-#     expect(my_array_finding_method(i_want_pets, "t")).to eq ["want","pets","but"]
-#   end
+  it "selects all words with a 't' in the original order" do
+    expect(my_array_finding_method(i_want_pets, "t")).to eq ["want","pets","but"]
+  end
 
-#   it "selects all words with an 'a' in the original order" do
-#     expect(my_array_finding_method(i_want_pets, "a")).to eq ["want","have"]
-#   end
+  it "selects all words with an 'a' in the original order" do
+    expect(my_array_finding_method(i_want_pets, "a")).to eq ["want","have"]
+  end
 
-# end
+end
 
-# describe 'PERSON 1: my_hash_finding_method' do
+describe 'PERSON 1: my_hash_finding_method' do
 
-#   let(:my_family_pets_ages) {{"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0, "Ditto" => 3}}
+  let(:my_family_pets_ages) {{"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0, "Ditto" => 3}}
 
-#   it "returns all names of pets who are 3 years old" do
-#     expect(my_hash_finding_method(my_family_pets_ages, 3)).to eq ["Hoobie", "Ditto"]
-#   end
+  it "returns all names of pets who are 3 years old" do
+    expect(my_hash_finding_method(my_family_pets_ages, 3)).to eq ["Hoobie", "Ditto"]
+  end
 
-#   it "returns all names of pets who are less than one year old" do
-#     expect(my_hash_finding_method(my_family_pets_ages, 0)).to eq ["Annabelle"]
-#   end
-# end
+  it "returns all names of pets who are less than one year old" do
+    expect(my_hash_finding_method(my_family_pets_ages, 0)).to eq ["Annabelle"]
+  end
+end
 
 # PERSON 2: MODIFY EXISTING DATA
 describe 'PERSON 2: my_array_modification_method!' do
@@ -55,83 +55,83 @@ describe 'PERSON 2: my_hash_modification_method!' do
 
 end
 
-# # PERSON 3: SORT DATA
-# describe 'PERSON 3: my_array_sorting_method!' do
-#   let(:i_want_pets) { ["I", "want", 3, "pets", "but", "only", "have", 2 ] }
+# PERSON 3: SORT DATA
+describe 'PERSON 3: my_array_sorting_method!' do
+  let(:i_want_pets) { ["I", "want", 3, "pets", "but", "only", "have", 2 ] }
 
-#   it "converts sorts all elements alphabetically without altering changing data" do
-#     expect(my_array_sorting_method(i_want_pets)).to eq [2, 3, "I", "but", "have", "only", "pets", "want"]
-#   end
+  it "converts sorts all elements alphabetically without altering changing data" do
+    expect(my_array_sorting_method(i_want_pets)).to eq [2, 3, "I", "but", "have", "only", "pets", "want"]
+  end
 
-#   it "operates non-destructively" do
-#      expect(my_array_sorting_method(i_want_pets).object_id).to_not eq(i_want_pets.object_id)
-#   end
+  it "operates non-destructively" do
+     expect(my_array_sorting_method(i_want_pets).object_id).to_not eq(i_want_pets.object_id)
+  end
 
-# end
+end
 
-# describe 'PERSON 3: my_hash_sorting_method!' do
-#   let(:my_family_pets_ages) {{"Evi" => 6, "Ditto" => 2, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 5, "Annabelle" => 0}}
+describe 'PERSON 3: my_hash_sorting_method!' do
+  let(:my_family_pets_ages) {{"Evi" => 6, "Ditto" => 2, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 5, "Annabelle" => 0}}
 
-#   it "returns an array that orders each animal first based on age (youngest to oldest)" do
-#     expect(my_hash_sorting_method(my_family_pets_ages)).to eq([["Annabelle", 0], ["Ditto", 2], ["Hoobie", 3], ["Bogart", 4], ["Poly", 5], ["Evi", 6], ["George", 12]])
-#   end
+  it "returns an array that orders each animal first based on age (youngest to oldest)" do
+    expect(my_hash_sorting_method(my_family_pets_ages)).to eq([["Annabelle", 0], ["Ditto", 2], ["Hoobie", 3], ["Bogart", 4], ["Poly", 5], ["Evi", 6], ["George", 12]])
+  end
 
-#   it "operates non-destructively" do
-#      expect(my_hash_sorting_method(my_family_pets_ages).object_id).to_not eq(my_family_pets_ages.object_id)
-#   end
+  it "operates non-destructively" do
+     expect(my_hash_sorting_method(my_family_pets_ages).object_id).to_not eq(my_family_pets_ages.object_id)
+  end
 
-# end
+end
 
-# # PERSON 4: DELETE MATCHING DATA
-# describe 'PERSON 4: my_array_deletion_method!' do
-#   let(:i_want_pets) { ["I", "want", 3, "pets", "but", "only", "have", 2 ] }
-#   it "deletes all words that contain an 'a'" do
-#     expect(my_array_deletion_method!(i_want_pets, "a")).to eq ["I", 3, "pets", "but", "only", 2 ]
-#   end
+# PERSON 4: DELETE MATCHING DATA
+describe 'PERSON 4: my_array_deletion_method!' do
+  let(:i_want_pets) { ["I", "want", 3, "pets", "but", "only", "have", 2 ] }
+  it "deletes all words that contain an 'a'" do
+    expect(my_array_deletion_method!(i_want_pets, "a")).to eq ["I", 3, "pets", "but", "only", 2 ]
+  end
 
-#   it "operates destructively" do
-#      expect(my_array_deletion_method!(i_want_pets, "o").object_id).to eq(i_want_pets.object_id)
-#   end
+  it "operates destructively" do
+     expect(my_array_deletion_method!(i_want_pets, "o").object_id).to eq(i_want_pets.object_id)
+  end
 
-# end
+end
 
-# describe 'PERSON 4: my_hash_deletion_method!' do
+describe 'PERSON 4: my_hash_deletion_method!' do
 
-#   let(:my_family_pets_ages) {{"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0, "Ditto" => 3}}
+  let(:my_family_pets_ages) {{"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0, "Ditto" => 3}}
 
-#   it "deletes an animal based on name" do
-#     expect(my_hash_deletion_method!(my_family_pets_ages, "George")).to eq({"Evi" => 6, "Hoobie" => 3, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0, "Ditto" => 3})
-#   end
+  it "deletes an animal based on name" do
+    expect(my_hash_deletion_method!(my_family_pets_ages, "George")).to eq({"Evi" => 6, "Hoobie" => 3, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0, "Ditto" => 3})
+  end
 
-#   it "operates destructively" do
-#      expect(my_hash_deletion_method!(my_family_pets_ages, "George").object_id).to eq(my_family_pets_ages.object_id)
-#   end
+  it "operates destructively" do
+     expect(my_hash_deletion_method!(my_family_pets_ages, "George").object_id).to eq(my_family_pets_ages.object_id)
+  end
 
-# end
+end
 
-# # PERSON 5: SPLIT DATA
-# describe 'PERSON 5: my_array_splitting_method' do
-#   let(:i_want_pets) { ["I", "want", 3, "pets", "but", "only", "have", 2 ] }
+# PERSON 5: SPLIT DATA
+describe 'PERSON 5: my_array_splitting_method' do
+  let(:i_want_pets) { ["I", "want", 3, "pets", "but", "only", "have", 2 ] }
 
-#   it "successfully splits elements of an array into two separate arrays based on whether the element is an integer" do
-#     expect(my_array_splitting_method(i_want_pets)).to eq [[3,2], ["I", "want","pets", "but", "only", "have"]]
-#   end
+  it "successfully splits elements of an array into two separate arrays based on whether the element is an integer" do
+    expect(my_array_splitting_method(i_want_pets)).to eq [[3,2], ["I", "want","pets", "but", "only", "have"]]
+  end
 
-#   it "operates non-destructively" do
-#      expect(my_array_splitting_method(i_want_pets).object_id).to_not eq(i_want_pets.object_id)
-#   end
+  it "operates non-destructively" do
+     expect(my_array_splitting_method(i_want_pets).object_id).to_not eq(i_want_pets.object_id)
+  end
 
-# end
+end
 
-# describe 'PERSON 5: my_hash_splitting_method' do
-#   let(:my_family_pets_ages) {{"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0, "Ditto" => 3}}
+describe 'PERSON 5: my_hash_splitting_method' do
+  let(:my_family_pets_ages) {{"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0, "Ditto" => 3}}
 
-#   it "splits a hash into nested arrays based on the age of the pet's age" do
-#     expect(my_hash_splitting_method(my_family_pets_ages, 5)).to eq ([[["Hoobie", 3], ["Bogart", 4], ["Poly", 4], ["Annabelle", 0], ["Ditto", 3]], [["Evi", 6], ["George", 12]]])
-#   end
+  it "splits a hash into nested arrays based on the age of the pet's age" do
+    expect(my_hash_splitting_method(my_family_pets_ages, 5)).to eq ([[["Hoobie", 3], ["Bogart", 4], ["Poly", 4], ["Annabelle", 0], ["Ditto", 3]], [["Evi", 6], ["George", 12]]])
+  end
 
-#   it "operates non-destructively" do
-#      expect(my_hash_splitting_method(my_family_pets_ages, 5).object_id).to_not eq(my_family_pets_ages.object_id)
-#   end
+  it "operates non-destructively" do
+     expect(my_hash_splitting_method(my_family_pets_ages, 5).object_id).to_not eq(my_family_pets_ages.object_id)
+  end
 
-# end
+end
--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -1,101 +1,96 @@
 # Research Methods
 
-# I spent [] hours on this challenge.
+# I spent [1] hours on this challenge.
 
 i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
 my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
 
 # Person 1's solution
 def my_array_finding_method(source, thing_to_find)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  return source.select{|x| x.to_s.include?(thing_to_find)}
 end
 
 def my_hash_finding_method(source, thing_to_find)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  return source.select{|key, value| value == thing_to_find}.keys
 end
 
 # Identify and describe the Ruby method(s) you implemented.
 #
-#
-#
+# .select - the select method finds array elements or hash key/value pairs that match the criterion on the right
+# .to_s - converts any object to a string
+# .include? - figures out of a string includes a specified sub-string
+# .keys - returns an array of just the keys in a hash
 
 # Person 2
 def my_array_modification_method!(pets_array, number_of_pets)
-	pets_array.map do |i|
+	pets_array.map! do |i|
 		i.is_a?(Integer)? i += number_of_pets : i
 	end
 end
 
-# p my_array_modification_method!(i_want_pets, 1)
-
-
 def my_hash_modification_method!(pets_hash, years)
-  pets_hash.each { | k, v | pets_hash[k] = v + 2 }
+  pets_hash.each { | k, v | pets_hash[k] = v + years }
 end
 
-# p my_hash_modification_method!(my_family_pets_ages, 2)
-
-# Identify and describe the Ruby method(s) you implemented.
-# 
+# Identify and describe the Ruby method(s) you implemented
 #
-#
-
+# .map - Finds the contents of an array that meet certain criteria, and changes the original array using a code block.
+# .is_a? - Identifies whether an object is of a certain type
+# .each - Iterates through a hash or an array and performs a code block on each item
 
 # Person 3
 def my_array_sorting_method(source)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.sort{|a,b| a.to_s <=> b.to_s}
 end
 
 def my_hash_sorting_method(source)
-   source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.sort_by{|k,v| v}
 end
-
-# Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+# sort => uses the <=> (comparison operator) to sort the inputs. The advantage is that you can sort by any criteria if you pass in a block.
+# sort_by => Only takees in one arguement.The argument is used to do all the sorting on that one input
+# to_s => Turns the output into a string
 
 
 # Person 4
 def my_array_deletion_method!(source, thing_to_delete)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.each do |word|
+    if word.to_s.include? (thing_to_delete)
+    source.delete(word)
+    end
+  end
 end
 
 def my_hash_deletion_method!(source, thing_to_delete)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.each do |v|
+    source.delete_if {|k,v| k==thing_to_delete }
+    end
 end
 
-# Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
 
-
-# Person 5
-def my_array_splitting_method(source)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
-end
-
-def my_hash_splitting_method(source, age)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
-end
-
-# Identify and describe the Ruby method(s) you implemented.
-#
-#
+#delete: It deletes the key-value pair
+#delete_if: It deletes every element for which block evaluates to true
+# .include?(): returns true if any member of the enumerable equals the object
+# .include?(): look for any letter, sub-string, integer, float by inserting them in the parentheses.
 #
 
-
-# Release 1: Identify and describe the Ruby method you implemented. Teach your
+# Release 2: Identify and describe the Ruby method you implemented. Teach your
 # accountability group how to use the methods.
 #
+#.select - is used in a similar way as .each. The difference is that where the .each method
+# => does something to every single element of an array, the .select method just finds the
+# => key/value pairs of a hash or elements of an array that fall into the criterion specified on
+# => the far right in the curly bars.
 #
+#.to_s is a fairly easy method to implement. It's used turn any object into a string. 2.to_s will
+# => return the string "2".
 #
+#.include?() - put whatever integer, float, or sub-string, you're looking for within those parenthesis
+# => in front of the period put whatever collection of data you're looking through
+# => so it reads data_collection.include?("sub-string") where data_collection is some collection of data
+# => (or just a single piece of data).
+#
+#.keys - when the method is placed after a hash, returns an array made up of only the keys of that hash
 
 
-# Release 3: Reflect!
-# What did you learn about researching and explaining your research to others?
-#
-#
-#
-#
+# The only trick I have for going through the Ruby Docs is liberal use of ctrl + f to find
+# key words that I thought would be related to methods I wanted to use.
