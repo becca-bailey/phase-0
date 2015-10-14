f0e5107fe3e9bc19edc7f55e6a2b55c1057b5be5
Becca Nelson <beccanelson88@gmail.com>
2015-10-13 21:12:14 -0500

    Guessing game edits

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -1,8 +1,8 @@
 # Build a simple guessing game
 
 
-# I worked on this challenge [by myself, with: ].
-# I spent [#] hours on this challenge.
+# I worked on this challenge by myself.
+# I spent 1 hours on this challenge.
 
 # Pseudocode
 
