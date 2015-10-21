bc10f665e4fbd1af0e60e8450116559100547e0c
Becca Nelson <beccanelson88@gmail.com>
2015-10-20 20:18:40 -0500

    Adds GPS reflection

--- week-7/gps6/my_solution.rb
+++ week-7/gps6/my_solution.rb
@@ -120,66 +120,25 @@ class VirusPredictor
   
 
   def predicted_deaths
-  
-    
-#     density = 50
-#     percentage = 0.1
-    
-#     until density == 200
-#       if @population_density >= density
-#         number_of_deaths = (@population * percentage).floor
-#         density += 50
-#         percentage += 0.1
-#         puts density
-#       else number_of_deaths = (@population * 0.05).floor
-#       end
-#     end
-      
-#       until density >= 200
-#         density += 50
-#         percentage += 0.1
-#       end
-#       density += 500
-#     end
-#     number_of_deaths = (@population * percentage).floor
-      
-      
-
-    if @population_density >= 200
-      @number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      @number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      @number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      @number_of_deaths = (@population * 0.1).floor
-    else
-      @number_of_deaths = (@population * 0.05).floor
-    end
-    
-
-  end
-  
-  # This method uses the population density to determine the speed of spread.  If the population density is higher, the virus spreads more quickly.  The result is printed in months.
+    percentage = 0.4  
+    population = 200  
+    while @number_of_deaths == nil
+      if @population_density < 50
+        @number_of_deaths = (@population * 0.5).floor
+      elsif @population_density >= population
+        @number_of_deaths = (@population * percentage).floor
+      else
+        population -= 50; percentage -= 0.1
+      end 
+    end 
+  end 
 
   def speed_of_spread
-    # in months
-    # We are still perfecting our formula here. The speed is also affected
-    # by additional factors we haven't added into this functionality.
-    @speed = 0.0
-
-    if @population_density >= 200
-      @speed += 0.5
-    elsif @population_density >= 150
-      @speed += 1
-    elsif @population_density >= 100
-      @speed += 1.5
-    elsif @population_density >= 50
-      @speed += 2
-    else
-      @speed += 2.5
+    density = 200
+    months = 0.5
+    while @speed == nil
+      @population_density >= density ? @speed = months : density -= 50; months += 0.5 
     end
-
   end
 
 end
@@ -193,3 +152,27 @@ def national_virus_effects(data_source)
 end
 
 national_virus_effects(STATE_DATA)
+
+=begin
+
+What are the differences between the two different hash syntaxes shown in the state_data file?
+
+The outer hash uses a string as the key and a "hash rocket" syntax, while the inner hash uses symbols as keys and uses the symbol syntax.  Since symbols don't store information in the same way as variables, they are better for repetitive data like the data contained in state_data.
+
+What does require_relative do? How is it different from require?
+
+Require relative will import a file located in the same directory, and only requires the path to the file relative to the current location.
+
+What are some ways to iterate through a hash?
+
+You can iterate over a hash using a method like .each or .map, and specifying variables for the key and value.  
+
+When refactoring virus_effects, what stood out to you about the variables, if anything?
+
+There were some unnecessary variables, especially when calling the two methods inside virus_effects.  Because the arguments are instance variables, they can still be accessed inside the method without including them in the method call. 
+
+What concept did you most solidify in this challenge?
+
+I learned more about calling methods inside of other methods, which is something that had been confusing before.  Also, our guide helped us to understand modules and constants and how they are used in Ruby.
+
+=end
